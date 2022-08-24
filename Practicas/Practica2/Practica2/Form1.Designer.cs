6fdcf920e3e6694e4af720d3196bc6024a858030
JorgeGoytia37 <31255378+goytiaj6@users.noreply.github.com>
2022-08-23 21:18:40 -0500

    Add files via upload

---
+++ Practicas/Practica2/Practica2/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace Practica2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxIex = new System.Windows.Forms.TextBox();
+            this.txtBoxVL1 = new System.Windows.Forms.TextBox();
+            this.txtBoxVL2 = new System.Windows.Forms.TextBox();
+            this.bttnAdd = new System.Windows.Forms.Button();
+            this.bttnGG = new System.Windows.Forms.Button();
+            this.bttnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtBoxNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Generador de curva de Froelich\r\npara generadores DC";
+            // 
+            // txtBoxIex
+            // 
+            this.txtBoxIex.Location = new System.Drawing.Point(159, 48);
+            this.txtBoxIex.Name = "txtBoxIex";
+            this.txtBoxIex.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxIex.TabIndex = 1;
+            // 
+            // txtBoxVL1
+            // 
+            this.txtBoxVL1.Location = new System.Drawing.Point(159, 74);
+            this.txtBoxVL1.Name = "txtBoxVL1";
+            this.txtBoxVL1.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxVL1.TabIndex = 2;
+            // 
+            // txtBoxVL2
+            // 
+            this.txtBoxVL2.Location = new System.Drawing.Point(159, 100);
+            this.txtBoxVL2.Name = "txtBoxVL2";
+            this.txtBoxVL2.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxVL2.TabIndex = 3;
+            // 
+            // bttnAdd
+            // 
+            this.bttnAdd.Location = new System.Drawing.Point(173, 152);
+            this.bttnAdd.Name = "bttnAdd";
+            this.bttnAdd.Size = new System.Drawing.Size(75, 23);
+            this.bttnAdd.TabIndex = 6;
+            this.bttnAdd.Text = "Agregar";
+            this.bttnAdd.UseVisualStyleBackColor = true;
+            this.bttnAdd.Click += new System.EventHandler(this.BttnAdd_Click);
+            // 
+            // bttnGG
+            // 
+            this.bttnGG.Location = new System.Drawing.Point(173, 195);
+            this.bttnGG.Name = "bttnGG";
+            this.bttnGG.Size = new System.Drawing.Size(75, 34);
+            this.bttnGG.TabIndex = 7;
+            this.bttnGG.Text = "Graficar y Guardar";
+            this.bttnGG.UseVisualStyleBackColor = true;
+            this.bttnGG.Click += new System.EventHandler(this.BttnGG_Click);
+            // 
+            // bttnExit
+            // 
+            this.bttnExit.Location = new System.Drawing.Point(173, 235);
+            this.bttnExit.Name = "bttnExit";
+            this.bttnExit.Size = new System.Drawing.Size(75, 23);
+            this.bttnExit.TabIndex = 8;
+            this.bttnExit.Text = "Salir";
+            this.bttnExit.UseVisualStyleBackColor = true;
+            this.bttnExit.Click += new System.EventHandler(this.BttnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Corriente Iex:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Voltaje de Linea 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Voltaje de Linea 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(127, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Voltaje de Linea (Froelich):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(265, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Se guardo el dato: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(265, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Se guardo el dato: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(265, 103);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Se guardo el dato: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(265, 129);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Se guardo el dato: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(368, 51);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(368, 77);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 18;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(368, 103);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 19;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(368, 129);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 13);
+            this.label13.TabIndex = 20;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(12, 206);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(122, 13);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "Nombre del archivo xlsx:";
+            // 
+            // txtBoxNombre
+            // 
+            this.txtBoxNombre.Location = new System.Drawing.Point(15, 222);
+            this.txtBoxNombre.Name = "txtBoxNombre";
+            this.txtBoxNombre.Size = new System.Drawing.Size(119, 20);
+            this.txtBoxNombre.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 278);
+            this.Controls.Add(this.txtBoxNombre);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bttnExit);
+            this.Controls.Add(this.bttnGG);
+            this.Controls.Add(this.bttnAdd);
+            this.Controls.Add(this.txtBoxVL2);
+            this.Controls.Add(this.txtBoxVL1);
+            this.Controls.Add(this.txtBoxIex);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxIex;
+        private System.Windows.Forms.TextBox txtBoxVL1;
+        private System.Windows.Forms.TextBox txtBoxVL2;
+        private System.Windows.Forms.Button bttnAdd;
+        private System.Windows.Forms.Button bttnGG;
+        private System.Windows.Forms.Button bttnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtBoxNombre;
+    }
+}
+
---
+++ Practicas/Practica2/Practica2/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpreadsheetLight;
+using SpreadsheetLight.Charts;
+using MATLAB_Library;
+
+namespace Practica2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+
+            //Creamos las cabeceras para nuestros dados
+            sl.SetCellValue("C2", "Iex[A]");
+            sl.SetCellValue("D2", "VL (Calculado) [V]");
+            sl.SetCellValue("E2", "VL (Iex ->) [V]");
+            sl.SetCellValue("F2", "VL (Iex <-) [V]");
+        }
+        //Hacemos el llamado de la API/libreria del MIT para el manejo de archivos en Excel
+        readonly SLDocument sl = new SLDocument();
+        //Nuestra libreria
+        readonly Basics MATLAB = new Basics();
+
+        private void BttnExit_Click(object sender, EventArgs e)
+        {
+            //Cerramos la aplicacion
+            Application.Exit();
+        }
+
+        private void BttnGG_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nombre = txtBoxNombre.Text;
+                //Dimensiones de aspecto para el gráfico
+                double fChartHeight = 15.0;
+                double fChartWidth = 7;
+
+                //Creacion del objeto grafica de excel
+                SLChart chart;
+
+                //Modificaciones de los parametros de nuestro grafico a generar
+                chart = sl.CreateChart("C2", "D15");
+                chart.SetChartType(SLLineChartType.StackedLineWithMarkers);
+                chart.SetChartStyle(SLChartStyle.Style5);
+                chart.SetChartPosition(1, 9, 1 + fChartHeight, 9 + fChartWidth);
+                sl.InsertChart(chart);
+
+                //Guardado y nombrado de nuestro Excel creado
+                sl.SaveAs(nombre + ".xlsx");
+
+                MessageBox.Show("Archivo creado y guardado con exito");
+            }
+            catch
+            {
+
+            }
+        }
+        /*
+        //Arreglos de datos leidos en prueba de laboratorio
+        double[] Iex = new double[] { .48, .50, .60, .70, .80, .90, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6 };
+        double[] VL1 = new double[] { 49, 50, 57, 65, 71, 80, 87, 98, 103, 108, 115, 118, 125 };
+        double[] VL2 = new double[] { 52, 56, 64, 71, 78, 86, 94, 99, 105, 111, 115, 118, 125 };
+        */
+        //Arreglos de datos leidos en prueba de laboratorio
+        readonly static double[] Iex = new double[13];
+        readonly static double[] VL1 = new double[13];
+        readonly static double[] VL2 = new double[13];
+
+        //Índices para el menejo de la hoja de calculo
+        readonly int jColumn1 = 3;
+        readonly int jColumn4 = 4;
+        readonly int jColumn2 = 5;
+        readonly int jColumn3 = 6;
+        int iRow = 3;
+        int i = 0;
+
+        private void BttnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(i == Iex.Length)
+                {
+                    MessageBox.Show("Ya se han tomado todas las lecturas");
+                }
+                else
+                {
+                    //Conversion de los datos
+                    double IexD = Convert.ToDouble(txtBoxIex.Text);
+                    double VL1D = Convert.ToDouble(txtBoxVL1.Text);
+                    double VL2D = Convert.ToDouble(txtBoxVL2.Text);
+                    //Comenzamos a agregar los datos en los arreglos
+                    Iex[i] = IexD;
+                    VL1[i] = VL1D;
+                    VL2[i] = VL2D;
+                    //Insertamos la lectura de corriente
+                    sl.SetCellValue(iRow, jColumn1, Iex[i]);
+                    //Insertamos las lecturas de Voltaje con conrriente incremental
+                    sl.SetCellValue(iRow, jColumn2, VL1[i]);
+                    //Insertamos las lecturas de Voltaje con conrriente decremental
+                    sl.SetCellValue(iRow, jColumn3, VL2[i]);
+                    //Insertamos los valores calculados
+                    double Calculo = Froelich(IexD);
+                    sl.SetCellValue(iRow, jColumn4, Calculo);
+                    iRow++;
+                    i++;
+                    //Se muestran los datos agregados
+                    label10.Text = Convert.ToString(IexD);
+                    label11.Text = Convert.ToString(VL1D);
+                    label12.Text = Convert.ToString(VL2D);
+                    label13.Text = Convert.ToString(Calculo);
+                    //Limpiamos los campos
+                    txtBoxIex.Clear();
+                    txtBoxVL1.Clear();
+                    txtBoxVL2.Clear();
+                }
+                
+            }
+            catch
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Calculamos el voltaje de linea con la ecuacion de Froelich
+        /// </summary>
+        /// <param name="Iex"> Corriente leida </param>
+        /// <returns></returns>
+        private double Froelich(double Iex)
+        {
+            double output;
+            output = ((MATLAB.Prod(2.542, Iex)) / (MATLAB.Sum(2.489, Iex))) * (125.664);
+            output = Math.Round(output, 3);
+            return output;
+        }
+    }
+}
