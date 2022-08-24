a7ddc8bb6fff303c1d41a07e037f166e797b0923
JorgeGoytia37 <31255378+goytiaj6@users.noreply.github.com>
2022-08-23 20:30:18 -0500

    Add files via upload

---
+++ Consolas de Pruebas/Descenso/ProgramDescenso.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Descenso
+{
+    class ProgramDescenso
+    {
+        private static void Main(string[] _)
+        {
+            //Entradas
+            double q1 = 8 * Math.Pow(10, -6);
+            double q2 = 6 * Math.Pow(10, -6);
+            double q3 = 7 * Math.Pow(10, -6);
+            double d = 0.12;
+            double r1 = 0.11;  //Este es el valor aleatorio
+            double valor = 35;
+            double paso = .001;
+
+            //Se imprimen los datos en pantalla
+            Console.WriteLine("\nEl valor de q1 es: {0} [C]", q1);
+            Console.WriteLine("\nEl valor de q2 es: {0} [C]", q2);
+            Console.WriteLine("\nEl valor de q3 es: {0} [C]", q3);
+            Console.WriteLine("\nLa distancia total es: {0} [m]", d);
+            Console.WriteLine("\nEl valor aleatorio es: {0} [m]", r1);
+            Console.WriteLine("\nEl valor deseado es: {0} [N]", valor);
+            Console.WriteLine("\nEl valor del paso es: {0} [m]", paso);
+
+            //Hacemos el llamado de las funciones
+            //Se hace la primera iteracion de la función descenso
+            Descenso(q1, q2, q3, d, r1, valor, paso);
+
+            Console.Read();
+        }
+
+        private static double Fuerzas(double q1, double q2, double q3, double d, double r1)
+        {
+            //Se obtiene la difrencia entre d y el valor aleatorio
+            double r2 = d - r1;
+
+            double k = (9 * Math.Pow(10, 9));
+            //Se calculan ambas fuerzas
+            double F31 = (k * q1 * q3) / (Math.Pow(r1, 2));
+            double F32 = (k * q2 * q3) / (Math.Pow(r2, 2));
+            //Se obtiene la fuerza total
+            double FT = F31 - F32;
+
+            return FT;
+        }
+ 
+        private static void Descenso(double q1, double q2, double q3, double d, double r1, double valor, double paso)
+        {
+            //Error cuadratico inicial
+            double FT1;
+            double EC1;
+
+            do
+            {
+                //Error cuadratico inicial
+                FT1 = Fuerzas(q1, q2, q3, d, r1);
+                EC1 = Math.Pow((FT1 - valor), 2);
+
+                //Eligimos un punto superior y se calcula la fuerza en ese punto
+                double r1S = r1 + paso;
+                double FT2 = Fuerzas(q1, q2, q3, d, r1S);
+                double EC2 = Math.Pow((FT2 - valor), 2); //Se calcula el error en este punto
+
+                //Eligimos un punto inferior y se calcula la fuerza en ese punto
+                double r1I = r1 - paso;
+                double FT3 = Fuerzas(q1, q2, q3, d, r1I);
+                double EC3 = Math.Pow((FT3 - valor), 2); //Se calcula el error en este punto
+
+                //Calculamos las pendientes asociadas
+                double pend1 = Math.Truncate(((EC2 - EC1) / (r1S - r1)) * 1000) / 1000;
+                double pend2 = Math.Truncate(((EC1 - EC3) / (r1 - r1I)) * 1000) / 1000;
+
+                //Comparamos los errores
+                if (EC1 > EC2)
+                {
+                    r1 = Math.Truncate(r1S * 1000) / 1000;
+                    Console.WriteLine("\nEl valor de pendiente es: {0}", pend1);
+                    Console.WriteLine("\nEl valor de r1 ahora es: {0} [mm]", r1);
+                }
+
+                else if (EC1 > EC3)
+                {
+                    r1 = Math.Truncate(r1I * 1000) / 1000;
+                    Console.WriteLine("\nEl valor de pendiente es: {0}", pend2);
+                    Console.WriteLine("\nEl valor de r1 ahora es: {0} [mm]", r1);
+                }
+            } while (EC1 > 1E-25);
+        }
+    }
+}
---
+++ Consolas de Pruebas/MatrizErrores/MatrizErrores.cs
@@ -0,0 +1,65 @@
+﻿using MATLAB_Library;
+using SpreadsheetLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizErrores
+{
+    class MatrizErrores
+    {
+        static void Main(string[] _)
+        {
+            //Declaracion de MATLAB para el uso de la libreria
+            Basics MATLAB = new Basics();
+            //Hacemos el llamado de la API/libreria del MIT para el manejo de archivos en Excel
+            SLDocument sl = new SLDocument();
+            //Creamos el arreglo de elementos de X entre .1 y 1
+            double[] X = MATLAB.Linspace(.1, 1, .1);
+            //Declaramos los valores de pendientes y ordenadas al origen de cada una de las rectas
+            //Primera recta (ideal)
+            double m1 = 1;
+            double b1 = 0;
+            //Creamos el arreglo de la primera recta
+            double[] y1 = MATLAB.ProdDot(X, m1); //Producto de la pendiente
+            double[] Y1 = MATLAB.SumDot(y1, b1); //Sumando la ordenada
+            //Tenemos la recta ideal creada
+            //Creamos los índices de renglón y columna
+            int iRow = 2;
+            int jColumn = 2;
+            //Indicamos la distribución de nuestra hoja de cálculo
+            sl.SetCellValue("A1", "m2/b2");
+            //Creamos las variables de pendiente y ordenada
+            double m2;
+            double b2;
+            //Creamos las instancias para la creacion de la segunda recta
+            double[] y2 = new double[Y1.Length];
+            double[] Y2 = new double[Y1.Length];
+            double error;
+            //Creamos un ciclo para la creación de la segunda recta, teniendo dos ciclos,
+            //uno para pendientes y otro para ordenadas
+            //Comenzamos a rellenar la matriz
+            for (m2 = -2; m2 <= 2; m2 += .1)
+            {
+                for(b2 = -2; b2 <= 2; b2 += .1)
+                {
+                    //Creamos el arreglo de la segunta recta
+                    y2 = MATLAB.ProdDot(X, m2); //Producto de la pendiente
+                    Y2 = MATLAB.SumDot(y2, b2); //Sumando la ordenada
+                    error = MATLAB.ErrAccumPrc(Y1, Y2); //Se crea el error acumulado
+                    sl.SetCellValue(iRow, jColumn, error);
+                    jColumn++;
+                }
+                jColumn = 2;
+                iRow++;
+            }
+            //Guardado y nombrado de nuestro Excel creado
+            sl.SaveAs("MatrizDeErrores.xlsx");
+            //Mensaje final del programa
+            Console.WriteLine("Programa terminado, archivo creado con exito");
+            Console.ReadLine();
+        }
+    }
+}
